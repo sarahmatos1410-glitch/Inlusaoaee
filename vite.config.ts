c65d498497e75cb9ce6e91ad68620f84892ca23a
sarahmatos1410-glitch <sarahmatos1410@gmail.com>
2026-01-06 22:59:50 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // A base deve ser o nome do seu repositório no GitHub
   base: '/Inlusaoaee/',
   build: {
     outDir: 'dist',
-  },
-  server: {
-    port: 3000
+    assetsDir: 'assets',
+    sourcemap: false
   }
 });
